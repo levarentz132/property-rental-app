51f4b927f0a6c929800629a326437337ed0dca7c
Henrique Souza <henrique.asouza@outlook.com>
2023-12-21 21:46:33 -0300

    eslint improvements

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,19 @@
 module.exports = {
   root: true,
   plugins: ["unused-imports"],
-  extends: ["universe/native"],
+  extends: [
+    "universe/native",
+    "@brucesong/eslint-config-react-native",
+    "prettier",
+  ],
   rules: {
     "import/order": "off",
     "unused-imports/no-unused-imports": "error",
     "@typescript-eslint/no-unused-vars": "error",
     "no-console": "warn",
+    "react/function-component-definition": "off",
+    "@typescript-eslint/no-namespace": "off",
+    "react-hooks/exhaustive-deps": "off",
+    "react/no-children-prop": "off",
   },
 };
